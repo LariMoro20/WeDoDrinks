93ce20c48ee285219e11372e6ccd03bd52e17499
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-04-09 02:06:13 -0300

    Update Menu.vue

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="full-width flex justify-center">
+  <nav class="full-width flex justify-center desktop-only">
     <ul class="horizontal__menu no-padding no-margin">
       <li class="horizontal__menu-item">
         <router-link class="horizontal__menu-item-link" to="/"
@@ -25,6 +25,34 @@
       </li>
     </ul>
   </nav>
+  <q-btn color="primary" label="Menu" class="mobile-only full-width">
+    <q-menu fit>
+      <q-list>
+        <q-item clickable v-close-popup>
+          <router-link class="horizontalmobile__menu-item-link" to="/"
+            >Inicial</router-link
+          >
+        </q-item>
+        <q-item clickable v-close-popup>
+          <router-link class="horizontalmobile__menu-item-link" to="/categories"
+            >Drinks</router-link
+          >
+        </q-item>
+        <q-item clickable v-close-popup>
+          <router-link
+            class="horizontalmobile__menu-item-link"
+            to="/drinks/type/Non_Alcoholic/"
+            >Drinks não alcoólicos</router-link
+          >
+        </q-item>
+        <q-item clickable v-close-popup>
+          <router-link class="horizontalmobile__menu-item-link" to="/"
+            >Drink Aleatório</router-link
+          >
+        </q-item>
+      </q-list>
+    </q-menu>
+  </q-btn>
 </template>
 
 <script>
@@ -42,5 +70,10 @@ export default {};
 .horizontal__menu-item-link:active {
   text-decoration: none;
   color: white;
+}
+.horizontalmobile__menu-item-link,
+.horizontalmobile__menu-item-link:active {
+  text-decoration: none;
+  color: black;
 }
 </style>
